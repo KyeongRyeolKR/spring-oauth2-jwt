fd4ac454873e82b0049f45a51e6aab3ace77e6eb
김경렬 <rlarudfuf802@naver.com>
2024-03-27 00:22:07 +0900

    feat: 사용자 정보 DB 저장

--- src/main/java/com/example/springoauth2jwt/service/CustomOAuth2UseService.java
+++ src/main/java/com/example/springoauth2jwt/service/CustomOAuth2UseService.java
@@ -1,6 +1,9 @@
 package com.example.springoauth2jwt.service;
 
 import com.example.springoauth2jwt.dto.*;
+import com.example.springoauth2jwt.entity.User;
+import com.example.springoauth2jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -10,7 +13,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class CustomOAuth2UseService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -29,11 +35,32 @@ public class CustomOAuth2UseService extends DefaultOAuth2UserService {
 
         String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
 
-        UserDto userDto = new UserDto();
-        userDto.setUsername(username);
-        userDto.setName(oAuth2Response.getName());
-        userDto.setRole("ROLE_USER");
+        User findUser = userRepository.findByUsername(username);
+        if(findUser == null) { // 첫 로그인(회원가입)
+            User newUser = new User();
+            newUser.setUsername(username);
+            newUser.setName(oAuth2Response.getName());
+            newUser.setEmail(oAuth2Response.getEmail());
+            newUser.setRole("ROLE_USER");
 
-        return new CustomOAuth2User(userDto);
+            userRepository.save(newUser);
+
+            UserDto userDto = new UserDto();
+            userDto.setUsername(username);
+            userDto.setName(oAuth2Response.getName());
+            userDto.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDto);
+        } else { // 기존 회원(바뀐 정보가 있다면 업데이트)
+            findUser.setEmail(oAuth2Response.getEmail());
+            findUser.setName(oAuth2Response.getName());
+
+            UserDto userDto = new UserDto();
+            userDto.setUsername(findUser.getUsername());
+            userDto.setName(findUser.getName());
+            userDto.setRole(findUser.getRole());
+
+            return new CustomOAuth2User(userDto);
+        }
     }
 }
