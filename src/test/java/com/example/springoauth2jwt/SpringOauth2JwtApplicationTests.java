805ba99953fb23c7b7363d9ece064cb139ac9e9b
김경렬 <rlarudfuf802@naver.com>
2024-03-26 23:29:40 +0900

    init

---
+++ src/test/java/com/example/springoauth2jwt/SpringOauth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springoauth2jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
